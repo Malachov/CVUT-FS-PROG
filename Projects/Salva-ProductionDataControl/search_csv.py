c94e07769adc2efb13677fab287a051bad38cbb9
Ondrej Salva <ondrej.salva@fs.cvut.cz>
2021-11-12 20:35:09 +0100

    commit

--- Projects/Salva-ProductionDataControl/search_csv.py
+++ Projects/Salva-ProductionDataControl/search_csv.py
@@ -1,9 +1,10 @@
-import analyze
 import os
 import datetime as dt
+import pandas as pd
 
 now = dt.datetime.now()
 ago = now - dt.timedelta(days=1)
+li = pd.DataFrame()
 
 for root, dirs, files in os.walk("data"):
     for fname in files:
@@ -11,5 +12,12 @@ for root, dirs, files in os.walk("data"):
         st = os.stat(path)
         mtime = dt.datetime.fromtimestamp(st.st_mtime)
         if mtime > ago:
-            analyze.analyze_threshold(path)
-            # print("%s modified %s" % (path, mtime))
+            # analyze.analyze_threshold(path)
+            print("%s modified %s" % (path, mtime))
+            col_list = ["teplota"]
+            df = pd.read_csv(path, index_col=None, header=0, usecols=col_list)
+            li = li.append(df, ignore_index=True)
+
+# frame = pd.concat(li, axis=0, ignore_index=True)
+li.reset_index()
+li.to_csv("data/analyze.csv")
