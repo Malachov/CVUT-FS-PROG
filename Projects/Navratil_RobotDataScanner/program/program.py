d1b86d3db50d22db63189337e9d387dd25056969
Tazeban <navrajan123@seznam.cz>
2021-11-30 12:09:49 +0100

    moje_novaverze

---
+++ Projects/Navratil_RobotDataScanner/program/program.py
@@ -0,0 +1,52 @@
+import pandas as pd
+
+
+
+
+def findtab(tabulka, cisloradku):
+#funkce hleda v danem souboru csv 
+# vstup = nazev souboru, cislo radku
+#vystup = hodnota na hledanem radku    
+    seznam=pd.read_csv(tabulka, index_col=None, dtype=str) #header=None,
+    seznam.set_index("id", inplace=True)
+    vysledek=seznam.loc[cisloradku, 'nazev']
+    return vysledek 
+
+
+while True:
+    scan = input("Zadej sestimistny kod:")
+   
+    if scan == "000000":
+        Konec=input("Nacten ukoncovaci kod. Program se nyni ukonci: (Y/N)")
+        if Konec == "Y" or Konec == "y":
+            print("zvoleno ukonceni programu, nashledanou")
+            break
+        else:
+            print("Program dale pokracuje")
+    elif len(scan) != 6:
+        print("Spatny format cisla!")
+    else:
+        print("To by mohlo jit")
+
+        trida=scan[:2]
+        # vzhledani tridy pomoci fce find
+        check=pd.read_csv("program/main.csv", index_col=None, dtype=str)
+        if (check.index == trida).any():
+            print("Tato trida neexistuje!")
+        else:
+            sekundar=findtab("program/main.csv", trida)
+            print(sekundar)
+            print(scan[2:])
+
+            check=pd.read_csv("program/" + sekundar + ".csv", index_col=None, dtype=str)
+            if (check.index == sekundar).any():
+                print("Tento prikaz neexistuje!")
+            else:
+                prikaz=findtab("program/" + sekundar + ".csv", scan[2:])
+                print(prikaz)
+                if prikaz[:2]=="00":
+                    prikaz[2:] + "()"
+
+
+
+print("Program byl uspesne ukoncen")
---
+++ Projects/Navratil_RobotDataScanner/program/scanner.py
@@ -0,0 +1,10 @@
+#knihovna programu pro scanner_change.csv
+import pandas as pd
+
+
+def save_start(kod):
+    save_info=pd.DataFrame({"nazev":[]},{"id":[]})
+    print(save_info)
+
+save_start(None)
+
