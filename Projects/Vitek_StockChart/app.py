b8ef02df7672c005987dcc429d4d20ee1834f933
LAKY911 <lukas.vitek911@gmail.com>
2022-11-10 22:24:05 +0100

    cant see candles, but change of ticker and days working

--- Projects/Vitek_StockChart/app.py
+++ Projects/Vitek_StockChart/app.py
@@ -28,14 +28,16 @@ class Window(QWidget):
         #print(self.stock_ticker)
     
     def update_stock(self) -> None:
-        #self.series.clear()
-        self.tm = []
-        #self.chart.removeAllSeries()
         self.downloadData()
+        #self.chartview.update()
+        #self.series.append(QCandlestickSet(120,150,105,130))
+        #self.axisX.append(str('12.11.'))
     
     def change_market_days(self, days: int) -> None:
         self.market_days_shown = days
-        print(self.market_days_shown)
+        #print(self.market_days_shown)
+        self.processData()
+        #self.chartview.update()
 
     stock_ticker = 'AAPL'
     market_days_shown = 20
@@ -56,15 +58,14 @@ class Window(QWidget):
             print("downloaded")
     
     def processData(self) -> None:
-        self.series = QCandlestickSeries()
-        self.series.setDecreasingColor(QColor(255,0,0))
-        self.series.setIncreasingColor(QColor(0,255,0))
+        self.series.clear()
+        self.axisX.clear()
+        self.tm = []
 
         y_min, y_max = (
             float("inf"),
             -float("inf"),
         )
-
         # process data
         for index, row in self.df.iterrows():
             self.series.insert(0,QCandlestickSet(row['open'], row['high'], row['low'], row['close']))
@@ -74,30 +75,18 @@ class Window(QWidget):
             y_max = max(y_max, row['open'], row['high'], row['low'], row['close'])
             if index == self.market_days_shown-1: break
         
-
-        
-
-        self.chart = QChart()
-        self.chart.addSeries(self.series)  # candles
-        self.chart.setAnimationOptions(QChart.AnimationOption.SeriesAnimations)
-        self.chart.legend().hide()
-        
-        
-        self.axisY = QValueAxis()   # axis Y
         self.axisY.setMax(y_max)
         self.axisY.setMin(y_min)
-        self.axisY.show()
-        self.chart.addAxis(self.axisY, Qt.AlignmentFlag.AlignLeft)
+        
+        self.axisX.append(self.tm)
+        
         
 
-        self.axisX = QBarCategoryAxis() # axis X
-        self.axisX.setLabelsAngle(-90)
-        self.axisX.append(self.tm)
-        self.axisX.show()
-        self.chart.addAxis(self.axisX, Qt.AlignmentFlag.AlignBottom)
-        self.series.attachAxis(self.axisX)
-        
-        self.chart.show()
+
+
+        #print("count:",self.series.count())
+        #self.chart.update()
+        #self.chart.show()
 
         
 
@@ -129,17 +118,43 @@ class Window(QWidget):
 
         self.vbox.addWidget(self.stock_symbol_container)
 
+        self.series = QCandlestickSeries()
+        self.series.setDecreasingColor(QColor(255,0,0))
+        self.series.setIncreasingColor(QColor(0,255,0))
+
+        self.chart = QChart()
+        self.chart.addSeries(self.series)  # candles
+        self.chart.setAnimationOptions(QChart.AnimationOption.SeriesAnimations)
+        self.chart.legend().hide()
+        
+        self.axisY = QValueAxis()   # axis Y
+        self.axisY.show()
+        self.chart.addAxis(self.axisY, Qt.AlignmentFlag.AlignLeft)
+
+        self.axisX = QBarCategoryAxis() # axis X
+        self.axisX.setLabelsAngle(-90)
+        self.axisX.show()
+        self.chart.addAxis(self.axisX, Qt.AlignmentFlag.AlignBottom)
+        self.series.attachAxis(self.axisX)
+
+
+
         self.downloadData()
+        self.chart.show()
 
         # VARIABLE GRID
         self.chartview = QChartView(self.chart)
+        self.chartview.setUpdatesEnabled(True)
         self.chartview.setObjectName("chartview")
         self.vbox.addWidget(self.chartview)
+
+        
 
         # MAIN LAYOUT
 
         self.vbox.addWidget(QLabel("Vytvořil Lukáš Vítek, 2022"))
         self.setLayout(self.vbox)
+
 
 
 if __name__ == "__main__":
